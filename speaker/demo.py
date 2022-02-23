915b0c46c51e8c19f9a9ff72b7cb881c683c594b
Jerry Wu <jerrywuzijie@gmail.com>
2022-02-23 12:23:35 -0500

    feat: demo.py to illustrate the use of speaker module

---
+++ speaker/demo.py
@@ -0,0 +1,29 @@
+"""
+author: Jerry Wu
+
+This file shows a simple demo of how to use speaker.py module
+"""
+
+import time
+from speaker import Speaker
+
+# initialize speaker object
+speaker = Speaker()
+
+# set the sound you want to play
+speaker.set_sound("sound/Let Her Go.mp3")
+
+# start playing, non-blocking. The sound will stop if program ends
+speaker.play_sound()
+print("Initial volume:", speaker.volume())
+print("\n" + "-"*20 +
+    "\nPress 'q' to exit\nPress 'up arrow' to increase volume'\nPress 'down arrow' to decrease volume\n"
+    + "-"*20 + "\n")
+
+try:
+    while speaker.is_playing() and speaker.get_keypress():
+        pass
+except KeyboardInterrupt:
+    print("Ctrl C pressed")
+finally:
+    print("stop playing, exit")
