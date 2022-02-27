b5a1c1df9c24cd84508b340c46c80586c70fc132
Jerry Wu <jerrywuzijie@gmail.com>
2022-02-27 18:17:57 -0500

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='go_to_bed',
+    version='1.0',
+    description='A useful module',
+    author='Jerry Wu, Yingzhuo Yang',
+    author_email='zw1711@nyu.edu, yy3826@nyu.edu',
+    url='https://github.com/JerryWuZiJie/go_to_bed',
+    install_requires=['pygame>=1.9.4.post1'],
+    py_modules=['go_to_bed'],
+)
