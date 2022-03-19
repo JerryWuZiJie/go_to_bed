97d41a5c815d5de5ccd51e7595302c8e34e19f5d
JerryWuZiJie <jerrywuzijie@gmail.com>
2022-03-19 17:55:32 -0400

    docs(setup.py): add new requirements for LED

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Jerry Wu, Yingzhuo Yang',
     author_email='zw1711@nyu.edu, yy3826@nyu.edu',
     url='https://github.com/JerryWuZiJie/go_to_bed',
-    install_requires=['pygame>=2.0.1', 'schedule'],
+    install_requires=['pygame>=2.1.2', 'schedule>=1.1.0',
+                      'luma>=2.1.4', 'luma.led_matrix>=1.6.1', 'pyttsx3>=2.90'],
     py_modules=['go_to_bed'],
 )
