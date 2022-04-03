49e58cbb936347ee712a44f8fc28060abc3bcf28
JerryWuZiJie <jerrywuzijie@gmail.com>
2022-04-02 20:58:10 -0400

    feat(app.py): implement OLED

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ all tasks to run are included in this file
 import os
 import time
 import threading
+from PIL import Image, ImageDraw, ImageFont
 
 import schedule
 import RPi.GPIO as GPIO
+import busio
+import board
+# usage: https://learn.adafruit.com/monochrome-oled-breakouts/python-usage-2
+import adafruit_ssd1306
 
 import go_to_bed
 
@@ -17,6 +22,17 @@ GPIO.setmode(GPIO.BCM)
 # setup RFID
 # setup ADC for photodiode
 # setup OLED
+OLED_WIDTH = 128
+OLED_HEIGHT = 64  ## TODO: might need to change to 32
+OLED_ADDRESS = 0x3c  ## TODO: might subject to changes
+# Initialize I2C library busio
+i2c = busio.I2C(board.SCL, board.SDA)  # TODO: board.I2C()?
+oled = adafruit_ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT,
+                                    i2c, addr=OLED_ADDRESS)
+# clear display
+oled.fill(0)
+oled.show()
+
 # setup push buttons
 # TODO: need to specify pins
 UP_PIN = 0
@@ -41,8 +57,6 @@ for (dirpath, dirnames, filenames) in os.walk("./sound"):
     available_files.extend(filenames)
 
 ### background tasks ###
-
-
 def run_webpage():
     """
     process that runs the webpage continuously
@@ -72,3 +86,33 @@ def alarm_clock():
     """
     # TODO
     pass
+
+
+### other functions ###
+def update_OLED():
+    """
+    update the OLED display
+    
+    # TODO
+    """
+    # Graphics stuff - create a canvas to draw/write on
+    image = Image.new("1", (oled.width, oled.height))  # "1" for 1 bit pixel
+    draw = ImageDraw.Draw(image)
+    # Draw a black rectangle (background) with 5 pixel background
+    draw.rectangle((0, 0, oled.width-1, oled.height-1),  # (x0, y0, x1, y1)
+                outline=255, fill=0,
+                width=5)
+    # Draw some text
+    font = ImageFont.load_default()
+    text = "Hello world"
+    font_width, font_height = font.getsize(text)
+    # TODO: draw.multiline_text()
+    draw.text(  # position text in center
+        (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+        text,
+        font=font,
+        fill=255,
+    )
+    # Display image
+    oled.image(image)
+    oled.show()
